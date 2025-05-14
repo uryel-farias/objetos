86e891c792f1b8fd76a7b161b68c5eab003e09b7
Uryel Farias <uryel.farias@gmail.com>
2025-05-14 11:38:33 -0300

    Objetos com funçoes

---
+++ objetos-e-funcoes.js
@@ -0,0 +1,10 @@
+const estudante = {
+  nome: 'José Silva',
+  idade: 32,
+  cpf: '12312312312',
+  turma: 'JavaScript',
+  bolsista: true,
+  telefones: ['551199999998', '551199999993'],
+  media: 7.5,
+  estaAprovado: (mediaBase) => this.media >= mediaBase ? true : false
+}
